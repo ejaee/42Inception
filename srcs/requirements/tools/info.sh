78399077c74b13e020a127128a98bd608dc25b41
ejaee <ejae8319@gmail.com>
2023-06-15 13:27:57 +0900

    refator: .sh

--- srcs/requirements/tools/info.sh
+++ srcs/requirements/tools/info.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-intra_id="ejachoi"
+# intra_id="ejachoi"
 volume_path="/home/ejachoi/data"
 if [ "$(uname)" == "Darwin" ]; then
     volume_path="/Users/ejachoi/goinfree/docker_study/inception2/data"
 fi
 wordpress_path="${volume_path}/wordpress"
 mariadb_path="${volume_path}/mariadb"
-# hosts_path="/etc/hosts"
-# conf="127.0.0.1 ejachoi.42.fr"
 
 if [ $# -eq 0 ]; then
     echo "NO ARGUMENTS" >&2
@@ -16,10 +14,8 @@ if [ $# -eq 0 ]; then
 fi
 
 case $1 in
-    "intra_id") echo "$intra_id" ;;
+    # "intra_id") echo "$intra_id" ;;
     "wordpress_path") echo "$wordpress_path" ;;
     "mariadb_path") echo "$mariadb_path" ;;
-    # "hosts_path") echo "$hosts_path" ;;
-    # "conf") echo "$conf" ;;
     *) echo "NOT FOUND INFO" >&2 ;;
 esac
