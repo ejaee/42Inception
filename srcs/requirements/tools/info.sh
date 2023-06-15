e262397edf7fc91f825d2d182bc2ea0283331e55
ejaee <ejae8319@gmail.com>
2023-06-15 13:15:27 +0900

    refator: .sh

--- srcs/requirements/tools/info.sh
+++ srcs/requirements/tools/info.sh
@@ -1,45 +1,25 @@
 #!/bin/bash
 
-intra_id=ejachoi
-
-# * ----------------------------------------------------------
+intra_id="ejachoi"
+volume_path="/home/ejachoi/data" # GNU/Linux platform을 기본으로 설정합니다.
 if [ "$(uname)" == "Darwin" ]; then
-    # Do something under Mac OS X platform        
-    volume_path=/Users/ejachoi/goinfree/docker_study/inception2/data # 이건 내 mac에서 테스트할 경로
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    # Do something under GNU/Linux platform
-    volume_path=/home/ejachoi/data
+    volume_path="/Users/ejachoi/goinfree/docker_study/inception2/data" # Mac OS X platform 경로
 fi
-# * ----------------------------------------------------------
-wordpress_path=${volume_path}/wordpress
-mariadb_path=${volume_path}/mariadb
-
-# * ----------------------------------------------------------
+wordpress_path="${volume_path}/wordpress"
+mariadb_path="${volume_path}/mariadb"
 hosts_path="/etc/hosts"
 conf="127.0.0.1 ejachoi.42.fr"
-# * ----------------------------------------------------------
 
-if [ $# -eq 0 ]
-then
-    echo "NO ARGUMENTS" 1>&2
+if [ $# -eq 0 ]; then
+    echo "NO ARGUMENTS" >&2
     exit 1
 fi
 
-if [ "intra_id" = $1 ]
-then
-    echo ejachoi
-# elif [ "wordpress_path" = $1 ]
-# then
-#     echo ${wordpress_path}
-# elif [ "mariadb_path" = $1 ]
-# then
-    # echo ${mariadb_path}
-elif [ "hosts_path" = $1 ]
-then
-    echo ${hosts_path}
-elif [ "conf" = $1 ]
-then
-    echo ${conf}
-else
-    echo "NOT FOUND INFO" 1>&2
-fi
+case $1 in
+    "intra_id") echo "$intra_id" ;;
+    "wordpress_path") echo "$wordpress_path" ;;
+    "mariadb_path") echo "$mariadb_path" ;;
+    "hosts_path") echo "$hosts_path" ;;
+    "conf") echo "$conf" ;;
+    *) echo "NOT FOUND INFO" >&2 ;;
+esac
