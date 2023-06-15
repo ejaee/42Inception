efaeaf153f24cea96da85d4c20e514a65c98bf1d
ejaee <ejae8319@gmail.com>
2023-06-15 15:36:17 +0900

    refator: .sh

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -4,9 +4,9 @@ GREEN='\033[1;32m'
 RESET='\033[0m'
 
 openssl req -newkey rsa:4096 -days 30 -nodes -x509 \
-    -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Lee/CN=ejachoi.42.fr" \
-    -keyout "/etc/ssl/ejachoi.42.fr.key" \
-    -out "/etc/ssl/ejachoi.42.fr.crt" 2>/dev/null
+    -subj "/C=KR/ST=Seoul/L=Seoul/O=42Seoul/OU=Lee/CN=${INTRA_ID}.42.fr" \
+    -keyout "/etc/ssl/${INTRA_ID}.42.fr.key" \
+    -out "/etc/ssl/${INTRA_ID}.42.fr.crt" 2>/dev/null
 
 cat <<EOM
 ${GREEN}--------------------
