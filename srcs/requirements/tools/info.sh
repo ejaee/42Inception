e6e0306e9018efd5be3825a6cac82026997d2f43
ejaee <ejae8319@gmail.com>
2023-06-15 13:32:55 +0900

    refator: .sh

--- srcs/requirements/tools/info.sh
+++ srcs/requirements/tools/info.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # intra_id="ejachoi"
-volume_path="/home/ejachoi/data"
-if [ "$(uname)" == "Darwin" ]; then
-    volume_path="/Users/ejachoi/goinfree/docker_study/inception2/data"
-fi
-wordpress_path="${volume_path}/wordpress"
-mariadb_path="${volume_path}/mariadb"
+# volume_path="/home/ejachoi/data"
+# if [ "$(uname)" == "Darwin" ]; then
+#     volume_path="/Users/ejachoi/goinfree/docker_study/inception2/data"
+# fi
+# wordpress_path="${volume_path}/wordpress"
+# mariadb_path="${volume_path}/mariadb"
 
-if [ $# -eq 0 ]; then
-    echo "NO ARGUMENTS" >&2
-    exit 1
-fi
+# if [ $# -eq 0 ]; then
+#     echo "NO ARGUMENTS" >&2
+#     exit 1
+# fi
 
-case $1 in
-    # "intra_id") echo "$intra_id" ;;
-    "wordpress_path") echo "$wordpress_path" ;;
-    "mariadb_path") echo "$mariadb_path" ;;
-    *) echo "NOT FOUND INFO" >&2 ;;
-esac
+# case $1 in
+#     # "intra_id") echo "$intra_id" ;;
+#     "wordpress_path") echo "$wordpress_path" ;;
+#     "mariadb_path") echo "$mariadb_path" ;;
+#     *) echo "NOT FOUND INFO" >&2 ;;
+# esac
--- srcs/requirements/tools/volume.sh
+++ srcs/requirements/tools/volume.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-wordpress_path=$($(pwd)/srcs/requirements/tools/info.sh wordpress_path)
-mariadb_path=$($(pwd)/srcs/requirements/tools/info.sh mariadb_path)
+volume_path="/home/ejachoi/data"
+if [ "$(uname)" == "Darwin" ]; then
+    volume_path="/Users/ejachoi/goinfree/docker_study/inception2/data"
+fi
+wordpress_path="${volume_path}/wordpress"
+mariadb_path="${volume_path}/mariadb"
+# wordpress_path=$($(pwd)/srcs/requirements/tools/info.sh wordpress_path)
+# mariadb_path=$($(pwd)/srcs/requirements/tools/info.sh mariadb_path)
 
 if [ ! -d ${wordpress_path} ]; then
     mkdir -p ${wordpress_path}
